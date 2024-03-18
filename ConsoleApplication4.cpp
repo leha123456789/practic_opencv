fa872e553579974a15cdf77718f01d15fa518808
Tengen <leha.denisov.03@mail.ru>
2024-03-18 10:01:56 +0300

    Update ConsoleApplication4.cpp

--- ConsoleApplication4.cpp
+++ ConsoleApplication4.cpp
@@ -1,8 +1,9 @@
-﻿#include <opencv2/core/core.hpp>
+
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <string>
+﻿#include <opencv2/core/core.hpp>
 using namespace cv;
 using namespace std;
 int main()
